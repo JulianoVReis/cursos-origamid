e40b60edf6ab9d3822d2b90370e3d9a1d2e6b0ae
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-01-14 12:56:40 -0300

    update

---
+++ javascript/exercícios/ex012/script.js
@@ -0,0 +1,21 @@
+// window.alert('Isso mesmo')
+
+const href = window.location.href
+
+console.log(href)
+
+if (href == 'http://127.0.0.1:5500/exerc%C3%ADcios/ex012/index.html') {
+	console.log('É igual')
+}
+
+const h1Selecionado = document.querySelector('h1')
+
+// alert('Teste')
+
+const h1Classes = h1Selecionado.classList
+
+function callbackh1() {
+	console.log('Clicou em ', h1Selecionado.innerText)
+}
+
+h1Selecionado.addEventListener('click', callbackh1)
