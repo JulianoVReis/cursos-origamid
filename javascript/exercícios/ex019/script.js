7886ad8c88b1327b8b4d50580bbcab618c9ee197
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-02-04 12:28:17 -0300

    update Cursos Origamid

--- javascript/exercícios/ex019/script.js
+++ javascript/exercícios/ex019/script.js
@@ -1,15 +1,41 @@
+// const h1 = document.querySelector('h1')
+// const animaisLista = document.querySelector('.animais-descricao')
+
+// console.log(h1.innerHTML)
+// console.log(h1.outerHTML)
+
+// // h1.outerHTML = '<p>Novo Título</p>'
+
+// // console.log(animaisLista.innerHTML)
+
+// const lista = document.querySelector('.animais-lista')
+
+// console.log(lista.children[--lista.children.length])
+// console.log(lista.querySelector('li:last-child'))
+// console.log(lista.previousSibling)
+
+// const animais = document.querySelector('.animais')
+// const contato = document.querySelector('.contato')
+// const titulo = document.querySelector('.titulo')
+
+// const mapa = document.querySelector('.mapa')
+
+// contato.replaceChild(mapa, titulo)
+
+// // lista.insertBefore(animais, mapa)
+
+// const novoh1 = document.createElement('h1')
+// novoh1.innerText = 'Novo Título'
+// novoh1.classList.add('titulo')
+
+// mapa.appendChild(novoh1)
+
+// console.log(novoh1)
+
 const h1 = document.querySelector('h1')
-const animaisLista = document.querySelector('.animais-descricao')
+const faq = document.querySelector('.faq')
 
-console.log(h1.innerHTML)
-console.log(h1.outerHTML)
+const cloneH1 = h1.cloneNode(true)
 
-// h1.outerHTML = '<p>Novo Título</p>'
-
-// console.log(animaisLista.innerHTML)
-
-const lista = document.querySelector('.animais-lista')
-
-console.log(lista.children[--lista.children.length])
-console.log(lista.querySelector('li:last-child'))
-console.log(lista.previousSibling)
+cloneH1.classList.add('azul')
+faq.appendChild(cloneH1)
