cba873e247fcb36ca23cde4a5e16a64cdaed16a6
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-02-17 13:28:20 -0300

    update

--- javascript/exercícios/ex028/script.js
+++ javascript/exercícios/ex028/script.js
@@ -42,20 +42,67 @@
 
 // console.log(moto)
 
-const moto = {
-	capacete: true
+// const moto = {
+// 	capacete: true
+// }
+
+// Object.defineProperties(moto, {
+// 	rodas: {
+// 		enumerable: true,
+// 		get() {
+// 			return 2
+// 		},
+// 		set(valor) {
+// 			this._rodas = valor * 4 + 'Total Rodas'
+// 		}
+// 	}
+// })
+
+// moto.rodas = 3
+// console.log(moto)
+
+// const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight')
+
+// Object.getOwnPropertyNames(Array.prototype)
+
+// console.log(Object.getPrototypeOf(''))
+// console.log(Array.prototype)
+
+// const frutas1 = ['Banana', 'Pêra']
+// const frutas2 = ['Banana', 'Pêra']
+
+// const novaFruta = frutas1
+
+// novaFruta[0] = 'Uva'
+
+// console.log(frutas1)
+
+// Object.is(frutas1, novaFruta)
+
+// const carro = {
+// 	marca: 'Ford',
+// 	ano: 2018
+// }
+
+// Object.freeze(carro)
+// carro.portas = 4
+// delete carro.marca
+
+// carro.marca = 'Honda'
+
+// console.log(carro)
+// console.log(Object.isFrozen(carro))
+
+const frutas = ['Banana', 'Uva']
+const frase = 'Oi frase'
+const somar = function(a, b) {
+	return a + b
 }
 
-Object.defineProperties(moto, {
-	rodas: {
-		get() {
-			return this._rodas
-		},
-		set(valor) {
-			this._rodas = valor * 4 + 'Total Rodas'
-		}
-	}
-})
+const carro = {
+	marca: 'Ford'
+}
 
-moto.rodas = 3
-console.log(moto)
+console.log(frutas.toString())
+
+console.log(Object.prototype.toString.call(somar))
