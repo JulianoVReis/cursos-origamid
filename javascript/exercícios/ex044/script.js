65b467b3acdb8af591630f831b167f2370e2af9f
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-03-13 12:33:44 -0300

    update javascript

---
+++ javascript/exercícios/ex044/script.js
@@ -0,0 +1,43 @@
+// function Button(text, background) {
+// 	this.text = text
+// 	this.background = background
+// }
+
+// Button.prototype.element = function() {
+// 	const buttonElement = document.createElement('button')
+// 	buttonElement.innerText = this.text
+// 	buttonElement.style.background = this.background
+// 	return buttonElement
+// }
+
+// class Button {
+// 	constructor(text, background, color) {
+// 		this.text = text
+// 		this.background = background
+// 		this.color = color
+// 		return this.element()
+// 	}
+// 	element() {
+// 		const buttonElement = document.createElement('button')
+// 		buttonElement.innerText = this.text
+//     buttonElement.style.background = this.background
+//     buttonElement.style.color = this.color
+// 		return buttonElement
+// 	}
+// 	appendTo(target) {
+// 		const targetElement = document.querySelector(target)
+// 		targetElement.appendChild(this.element())
+// 		console.log(this)
+// 		return targetElement
+// 	}
+// }
+
+// const blueButton = new Button('Comprar', 'blue', 'white')
+
+// console.log(blueButton.appendTo('body'))
+
+class Button {
+	constructor() {
+		
+	}
+}
