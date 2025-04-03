64ccc1c8422c3b74ad6b637402c8c8bd4e9d26c5
Juliano Reis <julianovreis@gmail.com>
2025-04-03 11:45:21 -0300

    update javascript

--- javascript/exercícios/ex062/js/script2.js
+++ javascript/exercícios/ex062/js/script2.js
@@ -6,9 +6,9 @@ import Tooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
-import initFetchAnimais from './modules/fetch-animais.js'
-import initFetchBitcoin from './modules/fetch-bitcoin.js'
-import initAnimacaoScroll from './modules/animacao-scroll.js'
+import fetchAnimais from './modules/fetch-animais.js'
+import fetchBitcoin from './modules/fetch-bitcoin.js'
+import ScrollAnima from './modules/scroll-anima.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init()
@@ -25,10 +25,13 @@ modal.init()
 const tooltip = new Tooltip('[data-tooltip]')
 tooltip.init()
 
-initTooltip()
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
+scrollAnima.init()
+
 initDropdownMenu()
 initMenuMobile()
 initFuncionamento()
-initFetchAnimais()
-initFetchBitcoin()
-initAnimacaoScroll()
+
+fetchAnimais('../animaisapi.json', '.numeros-grid')
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
