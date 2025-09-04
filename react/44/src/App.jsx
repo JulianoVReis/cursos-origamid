59512d97b8f8d3a09d6bb14a1ff5f3f5774fab1c
Juliano Reis <julianovreis@gmail.com>
2025-09-04 19:07:35 -0300

    update react

---
+++ react/44/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import foto from './img/foto.jpg'
+
+const App = () => {
+	return (
+		<div>
+			<p className='fundo'></p>
+			<img src={foto} alt="Cachorro" />
+		</div>
+	)
+}
+
+export default App
