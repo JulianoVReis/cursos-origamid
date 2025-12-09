ba1c329026c25c7ee81d016394068d7370944184
Juliano Reis <julianovreis@gmail.com>
2025-12-09 14:51:59 -0300

    update aula de react

--- react/55-projeto_final/src/Hooks/useForm.jsx
+++ react/55-projeto_final/src/Hooks/useForm.jsx
@@ -8,6 +8,10 @@ const types = {
 	password: {
 		regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
 		message: 'A senha deve conter ao menos 1 caractere maiúsculo, 1 caractere minúsculo e 1 dígito, com no mínimo 8 caracteres.'
+	},
+	number: {
+		regex: /^\d+$/,
+		message: 'Utilize números apenas.'
 	}
 }
 
