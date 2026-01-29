1839264d4b0956598ab48134f9bfcc36e6d4f3d5
Juliano Reis <julianovreis@gmail.com>
2026-01-29 15:13:39 -0300

    update aula de react

--- react/55-projeto_final/src/Components/User/User.jsx
+++ react/55-projeto_final/src/Components/User/User.jsx
@@ -4,13 +4,19 @@ import { Routes, Route } from 'react-router-dom'
 import Feed from '../Feed/Feed'
 import UserPhotoPost from './UserPhotoPost'
 import UserStats from './UserStats'
+import {UserContext} from '../../UserContext'
+
 
 const User = () => {
+	const {data} = React.useContext(UserContext)
+
+	if (!data) return null
+
 	return (
 		<section className='container'>
 			<UserHeader />
 			<Routes>
-				<Route path='/' element={<Feed />}/>
+				<Route path='/' element={<Feed user={data.id}/>}/>
 				<Route path='postar' element={<UserPhotoPost />}/>
 				<Route path='estatisticas' element={<UserStats />}/>
 			</Routes>
